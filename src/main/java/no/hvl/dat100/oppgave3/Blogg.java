b2fb932d335b4183681d3e947804b447976c9756
184893 <184893@stud.hvl.no>
2024-11-12 21:41:34 +0100

    oppg 3 og 4

--- src/main/java/no/hvl/dat100/oppgave3/Blogg.java
+++ src/main/java/no/hvl/dat100/oppgave3/Blogg.java
@@ -5,68 +5,122 @@ import no.hvl.dat100.oppgave1.*;
 
 public class Blogg {
 
-	// TODO: objektvariable 
+	private Innlegg[] innleggtabell;
+	private int nesteledig;
 
+	// Standardkonstruktør som setter startstørrelsen på tabellen til 20
 	public Blogg() {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		this.innleggtabell = new Innlegg[20];
+		this.nesteledig = 0;
 	}
 
+	// Konstruktør som setter startstørrelsen på tabellen til lengden spesifisert
 	public Blogg(int lengde) {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		this.innleggtabell = new Innlegg[lengde];
+		this.nesteledig = 0;
 	}
 
+	// Returnerer antall innlegg som er lagret i tabellen
 	public int getAntall() {
-		throw new UnsupportedOperationException(TODO.method());
+		return nesteledig;
 	}
 	
+	// Returnerer en peker til tabellen av innlegg-objekt
 	public Innlegg[] getSamling() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		return innleggtabell;
 	}
-	
+
+	// Finner indeks for et innlegg med samme id som gitt innlegg, eller returnerer -1
 	public int finnInnlegg(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		for (int i = 0; i < nesteledig; i++) {
+			if (innleggtabell[i].erLik(innlegg)) {
+				return i;
+			}
+		}
+		return -1;
 	}
 
+	// Returnerer true hvis et innlegg med samme id allerede finnes i samlingen
 	public boolean finnes(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+		return finnInnlegg(innlegg) != -1;
 	}
 
+	// Returnerer true hvis det er ledig plass i samlingen
 	public boolean ledigPlass() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		return nesteledig < innleggtabell.length;
 	}
 	
+	// Legger til et nytt innlegg hvis det ikke finnes fra før og det er ledig plass
 	public boolean leggTil(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		if (!finnes(innlegg) && ledigPlass()) {
+			innleggtabell[nesteledig] = innlegg;
+			nesteledig++;
+			return true;
+		}
+		return false;
 	}
 	
+	// Returnerer data i tabellen som en streng representasjon
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
+		StringBuilder sb = new StringBuilder();
+		sb.append(nesteledig).append("\n");
+
+		for (int i = 0; i < nesteledig; i++) {
+			sb.append(innleggtabell[i].toString());
+		}
+
+		return sb.toString();
 	}
 
-	// valgfrie oppgaver nedenfor
+	// Metoder nedenfor er valgfrie
 	
+	// Utvider innleggtabellen ved å øke dens lengde
 	public void utvid() {
-		throw new UnsupportedOperationException(TODO.method());
+		Innlegg[] nyTabell = new Innlegg[innleggtabell.length * 2];
+		for (int i = 0; i < nesteledig; i++) {
+			nyTabell[i] = innleggtabell[i];
+		}
+		innleggtabell = nyTabell;
 	}
 	
+	// Legger til innlegg og utvider tabellen om nødvendig
 	public boolean leggTilUtvid(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
-		
+		if (!finnes(innlegg)) {
+			if (!ledigPlass()) {
+				utvid();
+			}
+			innleggtabell[nesteledig] = innlegg;
+			nesteledig++;
+			return true;
+		}
+		return false;
 	}
 	
+	// Sletter et innlegg hvis det finnes i samlingen
 	public boolean slett(Innlegg innlegg) {
-		
-		throw new UnsupportedOperationException(TODO.method());
+		int pos = finnInnlegg(innlegg);
+		if (pos != -1) {
+			innleggtabell[pos] = innleggtabell[nesteledig - 1];
+			innleggtabell[nesteledig - 1] = null;
+			nesteledig--;
+			return true;
+		}
+		return false;
 	}
 	
+	// Søker etter innlegg som inneholder et gitt nøkkelord
 	public int[] search(String keyword) {
-		
-		throw new UnsupportedOperationException(TODO.method());
+		int[] result = new int[nesteledig];
+		int count = 0;
 
+		for (int i = 0; i < nesteledig; i++) {
+			if (innleggtabell[i].toString().contains(keyword)) {
+				result[count++] = i;
+			}
+		}
+
+		int[] trimmedResult = new int[count];
+		System.arraycopy(result, 0, trimmedResult, 0, count);
+		return trimmedResult;
 	}
 }
--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -2,14 +2,31 @@ package no.hvl.dat100.oppgave4;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-
-import no.hvl.dat100.common.TODO;
+import java.io.File;
 import no.hvl.dat100.oppgave3.*;
 
 public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
+		try {
+			// Oppretter filbanen ved å kombinere mappe og filnavn
+			File file = new File(mappe, filnavn);
+			PrintWriter writer = new PrintWriter(file);
 
-		throw new UnsupportedOperationException(TODO.method());
+			// Skriver ut antall innlegg først
+			writer.println(samling.getAntall());
+
+			// Skriver ut hvert innlegg i samlingen
+			for (Innlegg innlegg : samling.getSamling()) {
+				writer.print(innlegg.toString());
+			}
+
+			// Lukker skribenten etter å ha skrevet ut
+			writer.close();
+			return true;
+		} catch (FileNotFoundException e) {
+			System.out.println("Filen kunne ikke skrives: " + e.getMessage());
+			return false;
+		}
 	}
 }
